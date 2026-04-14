9d7c253264ece021971efb189793947a572ac8bc
Bottyán Vak <ecdl01@vbjnet.hu>
2026-04-14 10:13:54 +0200

    encryption

--- Program.cs
+++ Program.cs
@@ -2,8 +2,28 @@
 {
     internal class Program
     {
+        public static string encryption(string word,string key)
+        {
+            var letters = "abcdefghijklmnopqrstuvwxyz ";
+            string result = "";
+
+            if (key.Length < word.Length)
+                return result = "túl rövid kulcs";
+
+            for (int i = 0; i < word.Count(); i++)
+            {
+                var charcode = letters.IndexOf(key[i]) + letters.IndexOf(word[i]);
+                if (charcode > 26)
+                    result += letters[charcode % 27];
+                else result += letters[charcode];
+            }
+
+            return result;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine(encryption("helloworld", "abcdefgijkl"));
         }
     }
 }
